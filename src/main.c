c441263f961f7ec15b0ccac9acbd1e8b150b40e6
Jos Elkink <jelkink@gmail.com>
2024-11-03 18:30:23 +0000

    Add checking for options

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+  int c;
 
-  fprintf(stdout, "Hello, World!\n");
+  while ((c = getopt(argc, argv, "")) != -1)
+  {
+    switch(c)
+    {
+      default:
+        fprintf(stderr, "Usage: %s filename\n", argv[0]);
+        return 1;
+    }
+  }
 
+  if (optind < argc)
+  {
+    printf("Filename: %s\n", argv[optind]);
+  } else {
+    fprintf(stderr, "Filename required. Usage: %s filename\n", argv[0]);
+    return 1;
+  }
+  
   return(0);
 }
